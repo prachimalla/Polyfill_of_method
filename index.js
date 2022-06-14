58f65a14fa37f0468ef61e9c5d5d48eeb27205b1
prachimalla <prachimalla7@gmail.com>
2022-06-14 21:12:18 +0530

    1 file modified

--- index.js
+++ index.js
@@ -13,10 +13,8 @@ const name = {
 },
 
 function myName(state,home){
-  
   return this.first + this.last + ' '+state + ' ' + home  ;
 }
-
 const fullName = myName.bind(name);
 //console.log(fullName)
 
@@ -30,4 +28,31 @@ Function.prototype.myBind = function(...arg){
 
 const fullName2 = myName.myBind(name,'odisha');
 console.log(fullName2('bbsr'));
+
+
+//filter 
+//its return a new Array
+// for loop
+// check the condition with the each element
+const arr = [1,2,3,1,4,5,2,1,9,8];
+const fildata = arr.filter(data => data == 1
+);
+console.log(fildata);
+
+Array.prototype.myFilter = function(func){
+  if (this === void 0 || this === null) {
+    throw new TypeError();
+  }
+  let arr = this;
+  let retArr = []
+  
+ for(let i = 0 ;i<= arr.length ; i++){
+if(func(this[i],i,this)){
+  retArr.push(arr[i]);
+}
+ }
+return retArr;
+}
+console.log(arr.myFilter((data,index,arr) => data==1));
+
 
